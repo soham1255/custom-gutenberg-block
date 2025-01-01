c22e1161c53089dabfbc3d25d73de32bd35fd35e
Soham Patel <39306562+soham1255@users.noreply.github.com>
2025-01-01 22:25:36 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const { __ } = wp.i18n;
+const { registerBlockType } = wp.blocks;
+const { RichText } = wp.editor;
+
+registerBlockType( 'spcb-blocks/custom-block', {
+
+	title: __( 'Custom Block', 'custom-blocks' ),
+	icon: 'megaphone',
+	category: 'text',
+
+	attributes: {
+		fullName: {
+			type: 'array',
+			source: 'children',
+			selector: 'div'
+		},
+	},
+
+	edit: ( props ) => {
+		console.warn( 'props.attributes.fullName', props.attributes.fullName );
+		let { attributes: { fullName } , setAttributes, className } = props;
+		return (
+			<RichText
+				tagName="div"
+				placeholder={ __( 'Full Name', 'custom-blocks' ) }
+				value={ fullName }
+				onChange={ ( value ) => setAttributes( { fullName: value } ) }
+				className={ className }
+			/>
+		)
+	},
+	save: ( props ) => {
+		console.warn( 'save', props );
+		let { attributes: { fullName }, className } = props;
+		return (
+			<RichText.Content
+				tagName="div"
+				value={ fullName }
+				className={ className }
+			/>
+		)
+	}
+
+} );
